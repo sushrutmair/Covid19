20ac7449c2f8e497aedb6f8177faa1226f66c232
Sushrut J Mair <44080149+sushrutmair@users.noreply.github.com>
2020-05-22 15:33:06 +0530

    Changed node labels to a form of 'personname'+'nodeid' throughout. Tested.

--- datagen/cov19_con_trace.py
+++ datagen/cov19_con_trace.py
@@ -25,12 +25,12 @@ from copy import deepcopy
 pd.set_option('display.precision',12)
 
 #data file path. this is the data to be analyzed.
-datapath = 'cov19_gen_dataset_10.csv' #'cov19_gen_dataset_10k.csv'
+datapath = 'cov19_gen_dataset_05.csv' #'cov19_gen_dataset_10k.csv'
 
 #stores the size of the virtual microcell around each location a person was recorded to have visited.
 #this is used to calculate if two persons have breached the commonly accepted social distance limits.
 #can be changed to anything, default is kept at x metres. This is for tagging high risk contacts.
-microcell_radius = 0.003 # default is 0.003. It is about 10 ft captured here in metres
+microcell_radius = 0.005 # default is 0.003. It is about 10 ft captured here in metres
 
 #controls whether graphs are visually displayed or not. If running on linux ensure X Windows is available.
 #0 = graphs are displayed in ui. 1 = no graphs are displayed.
@@ -118,19 +118,31 @@ def graph_per_person(person):
     nodeid=0
     for index, row in one_persons_records.iterrows():
         #each recorded loc is a node
-        gx.add_node(nodeid,latlon=LatLon(Latitude(row['lat']),Longitude(row['lon'])))
+        nodelabel = str(person) + str(nodeid)
+        gx.add_node(nodelabel,latlon=LatLon(Latitude(row['lat']),Longitude(row['lon'])))
         nodeid = nodeid+1
     
+    noofnodes = nx.number_of_nodes(gx)
+
+    ###
+    for n,d in list(gx.nodes(data=True)):
+        print(n)
+        print(d)
+    ###
+
     #now let's add edges for the nodes
     print("Adding edges for: " + str(nx.number_of_nodes(gx)) + " nodes...")
     print(gx.nodes())
-    for x in range(0,nx.number_of_nodes(gx)):
+    for x in range(0,noofnodes):
         y = x + 1
-        if(y == nx.number_of_nodes(gx)):
+        if(y == noofnodes):
             print("reached end node")
             break
         else:
-            gx.add_edge(x,y,time=one_persons_records.at[y,'time'])
+            nodelabel1 = str(person) + str(x)
+            nodelabel2 = str(person) + str(y)
+            #gx.add_edge(nodelabel1,nodelabel2,time=one_persons_records.at[nodelabel2,'time'])
+            gx.add_edge(nodelabel1,nodelabel2,time=one_persons_records['time'][y])
 
     print("Completed adding edges for: " + str(person) + ". Graph complete.")
 
@@ -197,27 +209,29 @@ def find_overlap(undgx_curr, undgx_next):
     for x in range(0, len(gxcurr_nodeattrib)):
         for y in range(0, len(gxnext_nodeattrib)):
             #here, we compare curr(latlon) with next(latlon) iteratively.
-            print(str(gxcurr_nodeattrib[x]) + " ----- " + str(gxnext_nodeattrib[y]))
-            distance = gxcurr_nodeattrib[x].distance(gxnext_nodeattrib[y])
+            gxcurr_curr_nodelbl = str(anchorgraph_name) + str(x)
+            gxnext_curr_nodelbl = str(compargraph_name) + str(y)
+            print(str(gxcurr_nodeattrib[gxcurr_curr_nodelbl]) + " ----- " + str(gxnext_nodeattrib[gxnext_curr_nodelbl]))
+            distance = gxcurr_nodeattrib[gxcurr_curr_nodelbl].distance(gxnext_nodeattrib[gxnext_curr_nodelbl])
             print("Person: " + anchorgraph_name +  " & Person " + compargraph_name)
-            print("     - anchor node: " + str(x) + "  and comparison node: " + str(y))
+            print("     - anchor node: " + str(gxcurr_curr_nodelbl) + "  and comparison node: " + str(gxnext_curr_nodelbl))
             print("     - distance between above two: " + str(distance))
 
-            entm1 = find_startime_gx(x, undgx_curr)
-            extm1 = find_endtime_gx(x, undgx_curr)
+            entm1 = find_startime_gx(x, undgx_curr, anchorgraph_name)
+            extm1 = find_endtime_gx(x, undgx_curr, anchorgraph_name)
 
-            entm2 = find_startime_gx(y, undgx_next)
-            extm2 = find_endtime_gx(y, undgx_next)
+            entm2 = find_startime_gx(y, undgx_next, compargraph_name)
+            extm2 = find_endtime_gx(y, undgx_next, compargraph_name)
             
             risk = 'none'
             breach = 'no'
             if(distance <= microcell_radius):
+                
+                #@todo1 - a new edge connecting these two nodes and save the graph. Also mark
+                #the relevant loc's as 'breached' with a new node attribute. risk is still
+                #classified as none because we have not yet calculated time overlap
                 print("Microcell radius breached.")
                 breach = 'yes'
-
-                #@todo1 - a new edge connecting these two nodes and save the graph. Also mark
-                #the relevant loc's as 'overlapped' with a new node attribute. risk is still
-                #classified as none because we have not yet calculated time overlap
 
                 #@todo2 - time overlaps. use e*tm1 and e*tm2 to calculate overlap. If there is
                 #an overlap of time then we have two people in the same location at the same
@@ -226,7 +240,7 @@ def find_overlap(undgx_curr, undgx_next):
                 #the actual start time should be the time h and s were together first at this loc.
                 risk = 'high'
 
-                #@todo3 - since all h contacts to this newly infected h after that loc are suspect.
+                #@todo3 -  all h contacts to this newly infected h after that loc are suspect.
                 #this represents a med risk (s-->h1-->h2). We need to find all common_locs(h1,h2)
                 #and then time overlap. If common_loc && time_overlap then h2 is suspect at med risk.
                 #risk = 'med'
@@ -243,8 +257,10 @@ def find_overlap(undgx_curr, undgx_next):
                         'latlon2','entrytm2','exittm2','dist','breach', 'risk'])
                 b = b.append(data) """
             
-            data = pd.DataFrame([[anchorgraph_name, anchor_health_status, gxcurr_nodeattrib[x], entm1, extm1, 
-                    compargraph_name, compar_health_status, gxnext_nodeattrib[y], entm2, extm2, 
+            data = pd.DataFrame([[anchorgraph_name, anchor_health_status, 
+                    gxcurr_nodeattrib[gxcurr_curr_nodelbl], entm1, extm1, 
+                    compargraph_name, compar_health_status,
+                    gxnext_nodeattrib[gxnext_curr_nodelbl], entm2, extm2, 
                     distance, breach, risk]],
                     columns=['name1','con1','latlon1','entrytm1','exittm1','name2','con2',
                         'latlon2','entrytm2','exittm2','dist','breach', 'risk'])
@@ -252,42 +268,46 @@ def find_overlap(undgx_curr, undgx_next):
 
     return b
 
-def find_endtime_gx(nodeid, gx):
-    curr_node = nodeid
-    prev_node = 0
-    next_node = 0
-    if(nodeid == 0):
-        prev_node = 0
+#finds the exit time for the given graph's node. exit time = time when the person exited a recorded loc
+def find_endtime_gx(nodelabelsuffix, gx, nodelabelprefix):
+    curr_node = str(nodelabelprefix) + str(nodelabelsuffix)
+    prev_node_sfx = 0
+    next_node_sfx = 0
+    if(nodelabelsuffix == 0):
+        prev_node_sfx = 0
     else:
-        prev_node = nodeid-1
+        prev_node_sfx = nodelabelsuffix-1
 
-    if(nodeid ==len(gx)-1):
-        next_node = nodeid
+    if(nodelabelsuffix ==len(gx)-1):
+        next_node_sfx = nodelabelsuffix
     else:
-        next_node = nodeid + 1
+        next_node_sfx = nodelabelsuffix + 1
 
     extm1 = 0
+    next_node = str(nodelabelprefix) + str(next_node_sfx)
     if(gx.has_edge(curr_node,next_node)):
         extm1 = gx.get_edge_data(curr_node,next_node)
         extm1 = extm1[0]['time']
     
     return extm1
 
-def find_startime_gx(nodeid, gx):
-    curr_node = nodeid
-    prev_node = 0
-    next_node = 0
-    if(nodeid == 0):
-        prev_node = 0
+#finds the start time for the given graph's node. start time = time when a person entered a recorded loc
+def find_startime_gx(nodelabelsuffix, gx, nodelabelprefix):
+    curr_node = str(nodelabelprefix) + str(nodelabelsuffix)
+    prev_node_sfx = 0
+    next_node_sfx = 0
+    if(nodelabelsuffix == 0):
+        prev_node_sfx = 0
     else:
-        prev_node = nodeid-1
+        prev_node_sfx = nodelabelsuffix-1
 
-    if(nodeid ==len(gx)-1):
-        next_node = nodeid
+    if(nodelabelsuffix ==len(gx)-1):
+        next_node_sfx = nodelabelsuffix
     else:
-        next_node = nodeid + 1
+        next_node_sfx = nodelabelsuffix + 1
 
     entm1 = 0
+    prev_node = str(nodelabelprefix) + str(prev_node_sfx)
     if(gx.has_edge(prev_node,curr_node)):
         entm1 = gx.get_edge_data(prev_node,curr_node)
         entm1 = entm1[0]['time']
@@ -303,7 +323,9 @@ def test_all_graphs(g):
         print(" - Nodes:")
         print(g[i].nodes)
         for x1 in range(0, len(g[i].nodes)):
-            print("Node id: " + str(x1) + str(g[i].nodes[x1]))
+            nodelabel = str(g[i].graph['name']) + str(x1)
+            print("Node id: " + str(nodelabel) + str(g[i].nodes[nodelabel]))
+            #print("Node id: " + str(x1) + str(g[i].nodes[x1]))
         
         print(" - Edges:")
         print(g[i].edges)
@@ -320,7 +342,9 @@ def disp_graph(g):
         nx.draw_networkx_edge_labels(g, pos=nx.spring_layout(g))
         plt.show()
 
-##### main #####
+################
+##### MAIN #####
+################
 printcov("Starting Covid 19 contact tracing analysis for data in: ")
 printcov(" " + datapath)
 printcov("Configurations are: ")
@@ -329,7 +353,7 @@ print("Graph display control is: " + str(ui) + ".   0 = ON / 1 = OFF.")
 print('-------------------------------------')
 time.sleep(7.7)
 
-#call dataprep method
+#call dataprep method. We also get 'persons' during this
 sorteddf = dataprep()
 
 #call graph generation method for each person in the dataset
@@ -337,7 +361,6 @@ print("Initiating graph generation...")
 for person in range(0,len(persons)):
     graph_per_person(persons[person])
 
-#gxarry_pop_travel_hist was filled in graph_per_person
 test_all_graphs(gxarry_pop_travel_hist)
 
 travel_hist = overlaps_for_pop(gxarry_pop_travel_hist)
